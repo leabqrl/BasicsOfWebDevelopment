22a25fada27bbe280aaef4b2dd70ef7ab25b2ddb
leabqrl <lea.becquerel@gmail.com>
2025-12-12 16:33:52 +0200

    Add files via upload

---
+++ task-l/index.js
@@ -0,0 +1,9 @@
+// Accessibilité : focus sur les liens
+document.querySelectorAll('a').forEach(link => {
+  link.addEventListener('focus', () => {
+    link.style.outline = '2px solid #ff3131';
+  });
+  link.addEventListener('blur', () => {
+    link.style.outline = 'none';
+  });
+});
